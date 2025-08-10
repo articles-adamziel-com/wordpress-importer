88d2826d18b48f7ed05001fc590b6b4a681dc87b
Adam Zieliński <adam@adamziel.com>
2025-08-10 16:32:29 +0200

    Configure python.wasm to use WASI runtime

--- wasmtime/python-wasi.php
+++ wasmtime/python-wasi.php
@@ -1,10 +1,13 @@
 <?php
 $dir = __DIR__ . '/wasi-python';
-$wasm = file_get_contents($dir . '/bin/python3.wasm');
+$wasm = file_get_contents($dir . '/opt/wasi-python/bin/python3.wasm');
 $inst = new Wasmtime\Instance($wasm, [], [
     'wasi' => [
         'dir'  => $dir,
-        'env'  => ['PYTHONHOME' => '/'],
+        'env'  => [
+            'PYTHONHOME' => '/opt/wasi-python',
+            'PYTHONPATH' => '/opt/wasi-python/lib/python3.10',
+        ],
         'args' => ['python3', '-c', "print('Hello from python.wasm')"],
     ],
 ]);
