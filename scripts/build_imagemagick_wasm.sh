841cb63e6a4b91de12f15e726120e7c4d4f77880
Adam Zieliński <adam@adamziel.com>
2025-09-05 01:17:27 +0200

    Fix ImageMagick WASM build script

--- scripts/build_imagemagick_wasm.sh
+++ scripts/build_imagemagick_wasm.sh
@@ -13,6 +13,13 @@ ZLIB_VER=1.3.1
 LIBPNG_VER=1.6.43
 IMAGEMAGICK_VER=7.1.2-3
 
+# Ensure Emscripten tools are used for all builds
+export CC=emcc
+export CXX=em++
+export AR=emar
+export RANLIB=emranlib
+unset LIBS
+
 # Fetch and build zlib
 if [ ! -d zlib-$ZLIB_VER ]; then
   curl -LO https://zlib.net/zlib-$ZLIB_VER.tar.gz
@@ -43,11 +50,21 @@ if [ ! -d ImageMagick-$IMAGEMAGICK_VER ]; then
 fi
 cd ImageMagick-$IMAGEMAGICK_VER
 export CPPFLAGS="-I$BUILD_DIR/zlib-install/include -I$BUILD_DIR/libpng-install/include"
-export LDFLAGS="$BUILD_DIR/zlib-install/lib/libz.a $BUILD_DIR/libpng-install/lib/libpng16.a"
-emconfigure ./configure --disable-shared --enable-static --without-magick-plus-plus --without-perl --without-x --without-jpeg --host=wasm32-unknown-emscripten >/dev/null
+export LDFLAGS="-L$BUILD_DIR/zlib-install/lib -L$BUILD_DIR/libpng-install/lib"
+export LIBS="-lz -lpng16"
+# Remove stray configure lines that break under Emscripten
+sed -i "/^='-fPIC'/d" configure
+emconfigure ./configure \
+  --disable-shared \
+  --enable-static \
+  --without-magick-plus-plus \
+  --without-perl \
+  --without-x \
+  --without-jpeg \
+  --host=wasm32-unknown-emscripten >/dev/null
 emmake make >/dev/null
 
 # Link to wasm
-emcc MagickWand/.libs/libMagickWand-7.Q16HDRI.a MagickCore/.libs/libMagickCore-7.Q16HDRI.a $LDFLAGS -s STANDALONE_WASM=1 -o "$DIST_DIR/imagemagick.wasm"
+emcc MagickWand/.libs/libMagickWand-7.Q16HDRI.a MagickCore/.libs/libMagickCore-7.Q16HDRI.a $LDFLAGS $LIBS -s STANDALONE_WASM=1 -o "$DIST_DIR/imagemagick.wasm"
 
 printf "Built wasm module at %s/imagemagick.wasm\n" "$DIST_DIR"
